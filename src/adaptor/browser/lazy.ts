966b4cf38ce1e61d2735bb8f53cc61fe4f4d5041
Juan <juan.segnana@contractors.scale.com>
2022-11-03 10:14:08 -0300

    Fix import

--- src/adaptor/browser/lazy.ts
+++ src/adaptor/browser/lazy.ts
@@ -5,10 +5,17 @@
 import { getAll } from '../utils'
 
 export default async function adaptor() {
-  const { default: firebase } = await import('firebase/app')
-  await import('firebase/firestore')
+  // const { default: firebase } = await import('firebase/app')
+  const { 
+    getFirestore, 
+    DocumentReference, 
+    Timestamp, 
+    FieldPath, 
+    FieldValue, 
+    DocumentSnapshot 
+  } = await import('firebase/firestore')
 
-  const firestore = firebase.firestore()
+  const firestore = getFirestore()
   // At the moment, the browser's Firestore adaptor doesn't support getAll.
   // Get rid of the fallback when the issue is closed:
   // https://github.com/firebase/firebase-js-sdk/issues/1176
@@ -17,12 +24,12 @@ export default async function adaptor() {
   return {
     firestore,
     consts: {
-      DocumentReference: firebase.firestore.DocumentReference,
-      Timestamp: firebase.firestore.Timestamp,
-      FieldPath: firebase.firestore.FieldPath,
-      FieldValue: firebase.firestore.FieldValue
+      DocumentReference: DocumentReference,
+      Timestamp: Timestamp,
+      FieldPath: FieldPath,
+      FieldValue: FieldValue
     },
-    getDocMeta: (snapshot: firebase.firestore.DocumentSnapshot) => ({
+    getDocMeta: (snapshot: DocumentSnapshot) => ({
       fromCache: snapshot.metadata.fromCache,
       hasPendingWrites: snapshot.metadata.hasPendingWrites
     })
